1ff93e6386d1b4ffbea4c3ad402e2d7f2c8429b1
prokopCerny <5815801+prokopCerny@users.noreply.github.com>
2021-04-29 08:20:54 +0200

    Implement LastNCls encoders

--- scripts/pipeline_ict_train_embed_aetcn_experiment.py
+++ scripts/pipeline_ict_train_embed_aetcn_experiment.py
@@ -17,7 +17,8 @@ def get_run_name(two_tower: bool,
     return f'{encoder_type} {pretrained_checkpoint} {"2T" if two_tower else "1T"}{" fp16" if fp16 else ""} {dataset_name} Seed-{seed} T-len {target_max_seq_len} C-len {context_max_seq_len} Tr-batch {train_batch_size} Ev-b {eval_batch_size} O-dim {output_encode_dim}'
 
 if __name__ == '__main__':
-    ict_datasets_basepath = Path('/home/cernypro/dev/source/ml4logs/data/processed/Combined_20210401_roberta_only_target_flat_context')
+    # ict_datasets_basepath = Path('/home/cernypro/dev/source/ml4logs/data/processed/Combined_20210401_roberta_only_target_flat_context')
+    ict_datasets_basepath = Path('/home/cernypro/dev/source/ml4logs/data/processed/Combined_20210401_train_only_target_flat_context')
 
     datasets_to_embed_paths = [
         ('/home/cernypro/dev/source/ml4logs/data/interim/HDFS1/no_timestamps_test-data-HDFS1.log', '/home/cernypro/dev/source/ml4logs/data/interim/HDFS1/test-labels-HDFS1.csv'),
@@ -33,34 +34,54 @@ if __name__ == '__main__':
     seeds = [43]
     truncations = ['Smart_Average', 'Concat_To_Max']
 
-    # ict_datasets_full_paths = []
+    ict_datasets_full_paths = []
+
+    # ict_datasets_full_paths = [
+    #     (Path('/home/cernypro/dev/source/ml4logs/data/processed/train-data-HDFS1-cv1-1_roberta/flattened_contexts_epochs-4_seed-43_truncation-Concat_To_Max'), 'M_basic_chunked_10_roberta'),
+    #     (Path('/home/cernypro/dev/source/ml4logs/data/processed/train-data-HDFS1-cv1-1-time-ordered_roberta/flattened_contexts_epochs-4_seed-43_truncation-Concat_To_Max'), 'M_time_ordered_chunked_10_roberta')
+    # ]
 
     ict_datasets_full_paths = [
-        (Path('/home/cernypro/dev/source/ml4logs/data/processed/train-data-HDFS1-cv1-1_roberta/flattened_contexts_epochs-4_seed-43_truncation-Concat_To_Max'), 'M_basic_chunked_10_roberta'),
-        (Path('/home/cernypro/dev/source/ml4logs/data/processed/train-data-HDFS1-cv1-1-time-ordered_roberta/flattened_contexts_epochs-4_seed-43_truncation-Concat_To_Max'), 'M_time_ordered_chunked_10_roberta')
+        (Path('/home/cernypro/dev/source/ml4logs/data/processed/train-data-HDFS1-cv1-1_Epochs-4_Seed-43'), 'M_basic_chunked_10'),
+        (Path('/home/cernypro/dev/source/ml4logs/data/processed/train-data-HDFS1-cv1-1-time-ordered_Epochs-4_Seed-43'), 'M_time_ordered_chunked_10')
     ]
 
-    for epoch in epochs:
-        for seed in seeds:
-            for truncation in truncations:
-                dataset_folder_name = f'flattened_contexts_epochs-{epoch}_seed-{seed}_truncation-{truncation}'
-                dataset_path = ict_datasets_basepath / dataset_folder_name
-                dataset_run_name = f'Combined_20210401_roberta_{truncation.lower().replace("_", "-")}_epochs-{epoch}_seed-{seed}'
-                ict_datasets_full_paths.append((dataset_path, dataset_run_name))
+    # for epoch in epochs:
+    #     for seed in seeds:
+    #         for truncation in truncations:
+    #             dataset_folder_name = f'flattened_contexts_epochs-{epoch}_seed-{seed}_truncation-{truncation}'
+    #             dataset_path = ict_datasets_basepath / dataset_folder_name
+    #             dataset_run_name = f'Combined_20210401_roberta_{truncation.lower().replace("_", "-")}_epochs-{epoch}_seed-{seed}'
+    #             # dataset_run_name = f'Combined_20210401_{truncation.lower().replace("_", "-")}_epochs-{epoch}_seed-{seed}'
+    #             ict_datasets_full_paths.append((dataset_path, dataset_run_name))
     
     for dataset_path, dataset_run_name in ict_datasets_full_paths:
         for two_tower in [False, True]:
             target_max_seq_len = 512
             train_batch_size = 32
             output_encode_dim = 100
-            encoder_type = 'RobertaMean'
-            bert_model = 'distilroberta-base'
+            encoder_type = 'DistilbertLastNCls'
+            bert_model = 'distilbert-base-cased'
+            # encoder_type = 'RobertaLastNCls'
+            # bert_model = 'distilroberta-base'
+            # ict_job_num = run_train_ict_from_preprocessed(two_tower=two_tower,
+            #                                             encoder_type=encoder_type,
+            #                                             bert_model=bert_model,
+            #                                             dataset_run_name=dataset_run_name,
+            #                                             dataset_path=dataset_path,
+            #                                             eval_dataset_path='/home/cernypro/dev/source/ml4logs/data/processed/val-data-HDFS1-cv1-1-time-ordered_chunked-10_roberta/flattened_contexts_epochs-1_seed-43_truncation-Concat_To_Max',
+            #                                             output_encode_dim=output_encode_dim,
+            #                                             train_batch_size=train_batch_size,
+            #                                             target_max_seq_len=target_max_seq_len,
+            #                                             eval_steps=7500,
+            #                                             save_steps=5000,
+            #                                             logging_steps=100)
             ict_job_num = run_train_ict_from_preprocessed(two_tower=two_tower,
                                                         encoder_type=encoder_type,
                                                         bert_model=bert_model,
                                                         dataset_run_name=dataset_run_name,
                                                         dataset_path=dataset_path,
-                                                        eval_dataset_path='/home/cernypro/dev/source/ml4logs/data/processed/val-data-HDFS1-cv1-1-time-ordered_chunked-10_roberta/flattened_contexts_epochs-1_seed-43_truncation-Concat_To_Max',
+                                                        eval_dataset_path='/home/cernypro/dev/source/ml4logs/data/processed/val-data-HDFS1-cv1-1-time-ordered_chunked-10_distilbert',
                                                         output_encode_dim=output_encode_dim,
                                                         train_batch_size=train_batch_size,
                                                         target_max_seq_len=target_max_seq_len,
--- src/bert/encoders.py
+++ src/bert/encoders.py
@@ -66,6 +66,30 @@ class DistilBertForMeanEmbedding(DistilBertPreTrainedModel):
         return EmbeddingOutput(embedding=mean_encoding)
 
 
+class DistilBertForLastNClsEmbedding(DistilBertPreTrainedModel):
+    """
+    DistilBertModel with a linear layer applied to the last N [CLS] tokens.
+    Initialize using .from_pretrained(path_or_model_name) method
+    use task_specific_params={'cls_embedding_dimension': *YOUR EMBEDDING DIMENSION HERE*} to set embedding dimension
+    """
+    def __init__(self, config):
+        super().__init__(config)
+        if config.task_specific_params is None:
+            config.task_specific_params = dict()
+            
+        self.last_n = config.task_specific_params.setdefault('last_n_hidden_layers', 3)
+        self.distilbert = DistilBertModel(config)
+        self.concat_cls_projector = torch.nn.Linear(config.dim*self.last_n, config.task_specific_params.setdefault('cls_embedding_dimension', 100))
+
+        self.init_weights()
+    
+    def forward(self, input_ids, attention_mask):
+        bert_output = self.distilbert(input_ids=input_ids, attention_mask=attention_mask, output_hidden_states=True)
+        concat_cls_tokens = torch.cat([x[:, 0] for x in bert_output.hidden_states[-self.last_n:]], dim=1)
+        concat_cls_encoding = self.concat_cls_projector(concat_cls_tokens)
+        return EmbeddingOutput(embedding=concat_cls_encoding)
+
+
 class RobertaForClsEmbedding(RobertaPreTrainedModel):
     """
     RobertaModel with a linear layer applied to [CLS] token.
@@ -118,17 +142,29 @@ class RobertaForMeanEmbedding(RobertaPreTrainedModel):
         mean_encoding = self.mean_projector(mean_tokens_embedding)
         return EmbeddingOutput(embedding=mean_encoding)
 
-# possible method for doing masked mean along y -> output shape (x, z)
-# data - shape (x, y, z)
-# mask - shape (x, y)
-# data_masked = data * mask.unsqueeze(-1).float() # this zeroes out the vectors along y to not use
-# data_masked.sum(dim=1)/mask.sum(dim=1).unsqueeze(-1)  # sums up y, and then divides each vector by amount of unmasked elements
 
-# possible method to shorten mask on both sides, with each row of type [1 1 1 1 0 0], e.g. full of 1s up to a point and then 0s, with the goal being transforming the first and last 1s into zeros, e.g. for the example row have it become [0 1 1 0 0 0]
-# mask - shape (x, y)
-# mask[:, 0] = 0  # roll wraps around, have to zero-out first column, because it will become the last
-# mask = torch.roll(mask, -1, dims=1)
-# mask[:, 0] = 0 # zero-out the first column for real
+class RobertaForLastNClsEmbedding(RobertaPreTrainedModel):
+    """
+    RobertaModel with a linear layer applied to the last N [CLS] tokens.
+    Initialize using .from_pretrained(path_or_model_name) method
+    use task_specific_params={'cls_embedding_dimension': *YOUR EMBEDDING DIMENSION HERE*} to set embedding dimension
+    """
+    def __init__(self, config):
+        super().__init__(config)
+        if config.task_specific_params is None:
+            config.task_specific_params = dict()
+        
+        self.last_n = config.task_specific_params.setdefault('last_n_hidden_layers', 3)
+        self.roberta = RobertaModel(config, add_pooling_layer=False)
+        self.concat_cls_projector = torch.nn.Linear(config.hidden_size*self.last_n, config.task_specific_params.setdefault('cls_embedding_dimension', 100))
+
+        self.init_weights()
+    
+    def forward(self, input_ids, attention_mask):
+        bert_output = self.roberta(input_ids=input_ids, attention_mask=attention_mask, output_hidden_states=True)
+        concat_cls_tokens = torch.cat([x[:, 0] for x in bert_output.hidden_states[-self.last_n:]], dim=1)
+        concat_cls_encoding = self.concat_cls_projector(concat_cls_tokens)
+        return EmbeddingOutput(embedding=concat_cls_encoding)
 
 
 ### MUST BE LAST IN FILE, NOT REALLY CLEAN, BUT DONE FOR IMPORTING
@@ -136,5 +172,7 @@ KNOWN_ENCODER_CLASSES = {
     'DistilbertCls': DistilBertForClsEmbedding,
     'RobertaCls': RobertaForClsEmbedding,
     'DistilbertMean': DistilBertForMeanEmbedding,
-    'RobertaMean': RobertaForMeanEmbedding
+    'RobertaMean': RobertaForMeanEmbedding,
+    'DistilbertLastNCls': DistilBertForLastNClsEmbedding,
+    'RobertaLastNCls': RobertaForLastNClsEmbedding
 }
